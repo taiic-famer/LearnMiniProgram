d4a5524192ded981d6e148d67e560fae178e72b7
taiic-famer <2935718480@qq.com>
2020-12-24 08:59:23 +0800

    1.小程序初体验

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,7 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
+    name: "Coderwhy",
+    age: 18,
+    students:[
+      {id: 110, name: 'kobe', age: 30},
+      {id: 111, name: 'james', age: 28},
+      {id: 112, name: 'curry', age: 32},
+      {id: 113, name: 'why', age: 18},
+    ],
+    counter: 0
+  },
+  handleBtnClick(){
+    // 1.错误做法：界面不会刷新
+    // this.data.counter++
 
+    // 2.正确做法 this.setData()
+    this.setData({
+      counter: this.data.counter + 1
+    })
+  },
+  handleSubtraction(){
+    this.setData({
+      counter: this.data.counter - 1
+    })
   },
 
   /**
